a8ff57a9694cd969df1e3d08b0bb2835a34e4d12
Sichao Yu <syu25@ncsu.edu>
2020-08-08 03:43:04 +0800

    create aboutus component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Menu from './MenuComponent';
 import Dishdetail from "./DishdetailComponent"; 
+import About from "./AboutComponent";
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -49,6 +50,7 @@ class Main extends Component {
             <Route path="/home" component={HomePage} />
             <Route exact path ="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
             <Route path="/menu/:dishId" component={DishWithId} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
             <Route exact path="/contactus" component={Contact}/>
             <Redirect to="/home"/>
         </Switch>
